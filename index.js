2aa72eae49610c98d16788765559dc30ca7694cc
Your Name <vasipallieshan@gmail.com>
2026-09-20 21:51:21 +0800

    base with icons

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const { app, BrowserWindow, Menu } = require('electron')
 
 const createWindow = () => {
+  Menu.setApplicationMenu(null);
   const win = new BrowserWindow({
     width: 400,
     height: 686,
-    backgroundColor: '#00AAF1'
+    backgroundColor: '#00AAF1',
+    webPreferences:{
+      preload: path.join(__dirname, 'preload.js'),
+    },
+    icon: path.join(__dirname, 'icons', 'png', '256x256.png')
   })
 
   win.loadFile('src/index.html')
---
+++ preload.js
@@ -0,0 +1,4 @@
+const {contextBridge, shell} = require('electron');
+contextBridge.exposeInMainWorld('electronAPI',{
+    openExternal:(url)=> shell.openExternal(url)
+})
